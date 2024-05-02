b6737750fb09c8859d15c65a3f7c94074aa08495
mpfarias <fariasmauricio11@outlook.com>
2024-05-02 12:02:30 -0300

    Update ItemCard.tsx

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -3,14 +3,11 @@ import Button from "../utils/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 
-export default function ItemCard() {
+export default function ItemCard({ titleValue }: { titleValue: string }) {
   return (
     <div className="flex items-center justify-between border border-black mt-4 p-4">
-      <p>item</p>
-      <Button
-        className=""
-        label={<FontAwesomeIcon icon={faEllipsisVertical} />}
-      />
+      <p>{titleValue}</p>
+      <Button label={<FontAwesomeIcon icon={faEllipsisVertical} />} />
     </div>
   );
 }
