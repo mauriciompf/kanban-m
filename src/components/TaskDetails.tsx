6f49e1a38e86e9c43a871c26e3bb88932931f53d
mpfarias <fariasmauricio11@outlook.com>
2024-05-02 12:02:40 -0300

    Update TaskDetails.tsx

--- src/components/TaskDetails.tsx
+++ src/components/TaskDetails.tsx
@@ -2,25 +2,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 import Button from "../utils/Button";
+import React from "react";
 
 interface Props {
   setShowItem: (value: boolean) => void;
   setShowTaskDetails: (value: boolean) => void;
+  setTitleValue: (value: string) => void;
 }
 
 export default function TaskDetails({
   setShowItem,
   setShowTaskDetails,
+  setTitleValue,
 }: Props) {
+  // handle add item (task)
   const handleAddItem = () => {
     setShowTaskDetails(false);
     setShowItem(true);
+  };
+
+  const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    let titleValue = e.target.value;
+    setTitleValue(titleValue);
   };
 
   return (
     <div className="border-black border grid gap-4 p-4 mt-6">
       <div className="border-b border-black">
         <input
+          onChange={handleChangeTitle}
           type="text"
           placeholder="Title..."
           className="bg-transparent w-full"
@@ -39,7 +49,7 @@ export default function TaskDetails({
         </div>
 
         <Button
-          onClick={handleAddItem}
+          onClick={() => handleAddItem()}
           className={"bg-slate-500 p-1 rounded-lg font-bold w-full"}
           label={<FontAwesomeIcon icon={faCheck} />}
         />
