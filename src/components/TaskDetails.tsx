27284a7cf7ce1ba01a1a5926edd0c0dd333d1ca0
mpfarias <fariasmauricio11@outlook.com>
2024-05-05 19:31:16 -0300

    Update TaskDetails.tsx

--- src/components/TaskDetails.tsx
+++ src/components/TaskDetails.tsx
@@ -2,63 +2,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 import Button from "../utils/Button";
-import React from "react";
+import Priorities from "./Priorities";
 
-interface Props {
-  setShowTaskDetails: (value: boolean) => void;
-  showTaskDetails: boolean;
-  addItem: (title: string, priority: string) => void;
-  setTitleValue: (value: string) => void;
-  titleValue: string;
-  setPriority: (value: string) => void;
-  priority: string;
-}
+import { TaskDetailsProps } from "../utils/types";
 
 export default function TaskDetails({
   setShowTaskDetails,
-  showTaskDetails,
   addItem,
   setTitleValue,
   titleValue,
   setPriority,
   priority,
-}: Props) {
-  // handle add item (task)
+}: TaskDetailsProps) {
+  // Handle add item (task)
   const handleAddItem = () => {
     addItem(titleValue, priority);
     setShowTaskDetails(false);
+
+    // Reset title and priority
+    setTitleValue("");
+    setPriority("");
   };
 
   const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>): void =>
     setTitleValue(e.target.value);
-
-  const handlePriorityChange = (e: React.ChangeEvent<HTMLInputElement>): void =>
-    setPriority(e.target.value);
-
-  const Priorities = () => {
-    const prioritiesTags = ["A", "AA", "AAA"];
-
-    return (
-      <>
-        {prioritiesTags.map((item, index) => (
-          <div key={index} className="flex items-center gap-1 cursor-pointer">
-            <label className="cursor-pointer" htmlFor={`pri-${item}`}>
-              {item}
-            </label>
-            <input
-              type="radio"
-              className="cursor-pointer"
-              name="priority"
-              id={`pri-${item}`}
-              value={item}
-              checked={priority === item}
-              onChange={handlePriorityChange}
-            />
-          </div>
-        ))}
-      </>
-    );
-  };
 
   return (
     <div className="border-black border grid gap-4 p-4 mt-6">
@@ -80,7 +47,7 @@ export default function TaskDetails({
         <p>Priority: </p>
 
         <div className="flex gap-2 ">
-          <Priorities />
+          <Priorities setPriority={setPriority} priority={priority} />
         </div>
 
         <Button
