6798ee9e79ae3d2526d4aed1366188933a4899df
mpfarias <fariasmauricio11@outlook.com>
2024-05-07 11:54:47 -0300

    Update ItemCard.tsx

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -6,12 +6,18 @@ import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 export default function ItemCard({
   titleValue,
   priority,
+  onDragStart,
 }: {
   titleValue: string;
   priority: string;
+  onDragStart: any;
 }) {
   return (
-    <div className="flex items-center justify-between border border-black mt-4 p-4">
+    <div
+      className="flex items-center justify-between border border-black mt-4 p-4"
+      draggable
+      onDragStart={onDragStart}
+    >
       <div className="grid">
         <p>{titleValue}</p>
         {priority && <p>priority: {priority}</p>}
