7212fd53fbeb32729972ff027a7ab4faf93ee2c0
mpfarias <fariasmauricio11@outlook.com>
2024-05-08 16:42:23 -0300

    Update NewColumn.tsx

--- src/components/NewColumn.tsx
+++ src/components/NewColumn.tsx
@@ -4,11 +4,13 @@ import NewColumnButton from "../utils/NewColumnButton.tsx";
 
 const NewColumn = () => {
   const [columns, setColumns] = useState<JSX.Element[]>([]);
+  const [columnId, setColumnId] = useState(0);
 
   const addColumn = () => {
     setColumns([...columns, <Column key={columns.length} />]);
-  };
 
+    setColumnId(columnId + 1);
+  };
   return (
     <>
       {columns.map((column, index) => (
