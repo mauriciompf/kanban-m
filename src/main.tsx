8a43095a1c4ea2f1f51e7f0f9167d92ae5e213f4
mpfarias <fariasmauricio11@outlook.com>
2024-05-08 19:36:15 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <DndProvider backend={HTML5Backend}>
+      <App />
+    </DndProvider>
   </React.StrictMode>
 );
