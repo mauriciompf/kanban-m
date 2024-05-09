db57ec50e55e4d2b0f3fdb788f75fb603102abf7
mpfarias <fariasmauricio11@outlook.com>
2024-05-09 15:30:50 -0300

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
-// import Column from "./Column.tsx";
+import { DndContext } from "@dnd-kit/core";
 import NewColumn from "./NewColumn.tsx";
 
 export default function App() {
   return (
-    <main className="m-8 flex gap-4 items-start">
-      <NewColumn />
-    </main>
+    <DndContext>
+      <main className="m-8 flex gap-4 items-start">
+        <NewColumn />
+      </main>
+    </DndContext>
   );
 }
