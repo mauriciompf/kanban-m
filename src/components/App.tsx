90982408e4dbe1e40ffd571fc7ecb7123d705bfa
mpfarias <fariasmauricio11@outlook.com>
2024-05-20 19:01:33 -0300

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,59 +2,14 @@ import { useState } from "react";
 import Column from "./Column.tsx";
 
 export default function App() {
-  const [showTaskEdit, setShowTaskEdit] = useState<boolean>(false);
-  const [tasks, setTasks] = useState<string[]>([]);
-  const [title, setTitle] = useState<string>("");
   const [columns, setColumns] = useState<any[]>([]);
-
-  const handleShowEdit = () => {
-    setShowTaskEdit(!showTaskEdit);
-  };
-
-  const handleChangeTask = (e: any) => {
-    const value = e.target.value;
-    setTitle(value);
-  };
-
-  const addTask = () => {
-    setShowTaskEdit(false);
-    setTasks((tasks) => [title, ...tasks]);
-  };
-
-  const ColumnMarkup = (
-    <Column className="grid gap-4 border-black border p-2 w-[200px]">
-      {/* Add new task */}
-      <div>
-        <input type="text" placeholder="New Column" />
-        <button className="border border-black p-2" onClick={handleShowEdit}>
-          Add new task +
-        </button>
-      </div>
-
-      {/* Details task*/}
-      {showTaskEdit && (
-        <div className="flex gap-2 items-center justify-between">
-          <input type="text" onChange={handleChangeTask} className="w-full" />
-          <button onClick={addTask}>Add</button>
-        </div>
-      )}
-
-      {/* Card task*/}
-      {tasks.map((task, index) => (
-        <div key={index} className="border border-black p-2">
-          {task}
-        </div>
-      ))}
-    </Column>
-  );
-
   const addColumn = () => {
-    setColumns((columns: any) => [...columns, ColumnMarkup]);
+    setColumns((columns: any) => [...columns, <Column />]);
   };
 
   return (
     <main className="m-4 bg-blue-100 p-2">
-      <button onClick={addColumn} className="border border-black p-2">
+      <button onClick={addColumn} className="border mb-5 border-black p-2">
         Add new Column +
       </button>
 
